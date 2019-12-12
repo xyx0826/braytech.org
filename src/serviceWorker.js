54247a722e0adf756467bd7c7f950fca4d8a36ba
Tom Chapman <chapman@live.com.au>
2019-12-13 04:02:32 +1000

    Update serviceWorker.js

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -11,17 +11,15 @@
 // opt-in, read http://bit.ly/CRA-PWA.
 
 const isLocalhost = Boolean(
-  // window.location.hostname === 'localhost' ||
-  //   // [::1] is the IPv6 localhost address.
-  //   window.location.hostname === '[::1]' ||
-  //   // 127.0.0.1/8 is considered localhost for IPv4.
-  //   window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
-  1 === 2
+  window.location.hostname === 'localhost' ||
+    // [::1] is the IPv6 localhost address.
+    window.location.hostname === '[::1]' ||
+    // 127.0.0.1/8 is considered localhost for IPv4.
+    window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
 );
 
 export function register(config) {
-  // if (process.env.NODE_ENV === 'production' && process.env.REACT_APP_BETA === 'true' && 'serviceWorker' in navigator) {
-  if ('serviceWorker' in navigator) {
+  if (process.env.NODE_ENV === 'production' && process.env.REACT_APP_BETA === 'true' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location);
     if (publicUrl.origin !== window.location.origin) {
