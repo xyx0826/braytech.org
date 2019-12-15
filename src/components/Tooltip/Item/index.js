5f1057fcfd2d1e7d57508d7ceee84bdd35de2938
Tom Chapman <chapman@live.com.au>
2019-12-16 02:37:25 +1000

    2.12.31

--- src/components/Tooltip/Item/index.js
+++ src/components/Tooltip/Item/index.js
@@ -188,7 +188,7 @@ class Item extends React.Component {
       }
     }
 
-    const masterworked = enums.enumerateItemState(item.state).masterworked || item.masterwork?.stats?.filter(s => s.value >= 9).length;
+    const masterworked = enums.enumerateItemState(item.state).masterworked || (definitionItem.itemType === enums.DestinyItemType.Armor ? item.masterwork?.stats?.filter(s => s.value > 9).length : item.masterwork?.stats?.filter(s => s.value >= 9).length);
 
     console.log(item)
 
