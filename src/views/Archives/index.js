7893de5f576719c4d8332a65865b1fe5f6b5b0cb
Tom Chapman <chapman@live.com.au>
2020-01-24 18:08:45 +1000

    2.14.41

--- src/views/Archives/index.js
+++ src/views/Archives/index.js
@@ -4,12 +4,14 @@ import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { withTranslation } from 'react-i18next';
 import ReactMarkdown from 'react-markdown';
+import ReactMoment from 'react-moment';
 
 import manifest from '../../utils/manifest';
 import { ReactComponent as HighlightThisWeek } from '../../svg/miscellaneous/highlight-this-week.svg';
 import { markdown_linkHelper } from '../../utils/misc';
 
 import './styles.css';
+import './list-table.css';
 
 export function NavLinks(props) {
   return (
@@ -19,10 +21,10 @@ export function NavLinks(props) {
           <div className='icon' />
           <NavLink to='/archives' exact />
         </li>
-        <li className='linked'>
+        {/* <li className='linked'>
           <div className='icon manifest' />
           <NavLink to='/archives/manifest' exact />
-        </li>
+        </li> */}
         <li className='linked'>
           <div className='icon eververse' />
           <NavLink to='/archives/eververse' exact />
@@ -40,6 +42,27 @@ class Archives extends React.Component {
   render() {
     const { t } = this.props;
 
+    const entries = [
+      {
+        name: manifest.DestinyInventoryItemDefinition[1115550924].displayProperties.name,
+        description: t("???"),
+        deployed: "2019-06-10T00:00:00.000Z",
+        link: '/archives/chalice-of-opulence'
+      },
+      {
+        name: t('Eververse season overview'),
+        description: t("This view details each of the current season's weekly Eververse store stock.\n\nIts data comes directly from the API manifest's definition of _Tess Everis_ and is displayed by merely iterating over it."),
+        deployed: "2020-01-10T00:00:00.000Z",
+        link: '/archives/eververse'
+      },
+      {
+        name: t('Manifest tool'),
+        description: t("???"),
+        link: '/archives/manifest',
+        wip: true
+      }
+    ];
+
     return (
       <div className='view index' id='archives'>
         <div className='module head'>
@@ -56,18 +79,39 @@ class Archives extends React.Component {
               </div>
               <ReactMarkdown className='text' renderers={{ link: markdown_linkHelper }} source={t("Interactive tools, manuals, legends, and other content preserved by _The Archives_.\n\n_The Archives_ are where various, previously thought lost, repositories now reside. These repositories are containers to useful data and tools which originally coalesced during the height of humanity, commonly referred to as [_The Golden Age_](https://www.ishtar-collective.net/cards/the-golden-age).\n\nSometimes, not everything I build (or attempt to build for that matter) finds a home in the limelight of _Braytech's_ primary navigation, but still retains so much user value that it deserves a place to live. This is their space.\n\nPlease note, not everything here is guaranteed to work well, or at all. Some contents may even cause low-power devices to lock up under specific use coniditions i.e. the _Manifest_ tool.")} />
             </div>
-            {/* <div className='module highlights'>
-              <h3>{t('Eververse season overview')}</h3>
-              <ReactMarkdown className='text' source={t("View details of the current season's weekly Eververse store stock")} />
-              <Link className='button' to='/archives/eververse'>
-                <div className='text'>{t('Go to')}</div>
-              </Link>
-              <h3>{manifest.DestinyInventoryItemDefinition[1115550924].displayProperties.name}</h3>
-              <ReactMarkdown className='text' source={t('')} />
-              <Link className='button' to='/compare/nightfalls'>
-                <div className='text'>{t('Go to')}</div>
-              </Link>
-            </div> */}
+            <div className='module highlights'>
+              <ul className='list table'>
+                <li className='header'>
+                  <ul>
+                    <li className='col name no-sort' />
+                    <li className='col description no-sort'>
+                    <div className='full'>{t('Description')}</div>
+                    </li>
+                    <li className='col date no-sort'>
+                    <div className='full'>{t('Deployed')}</div>
+                    </li>
+                    <li className='col link no-sort' />
+                  </ul>
+                </li>
+                {entries.map((entry, i) => (
+                  <li key={i}>
+                    <ul>
+                      <li className='col name'>{entry.name}</li>
+                      <li className='col description'><ReactMarkdown className='text' source={entry.description} /></li>
+                      <li className='col date optional'>
+                        {entry.deployed && <ReactMoment format='MMMM YYYY'>{entry.deployed}</ReactMoment>}
+                      </li>
+                      <li className='col link'>
+                        <Link className='button' to={entry.link}>
+                          <div className='text'>{entry.wip ? 'WIP' : t('Launch')}</div>
+                          <i className='segoe-uniE0AB' />
+                        </Link>
+                      </li>
+                    </ul>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
       </div>
